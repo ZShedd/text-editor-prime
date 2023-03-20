b1bcea057599bc23d378ecd6a254c95b132ddedc
Clown Supreme <zachary.h.shedd@gmail.com>
2023-03-19 21:11:46 -0700

    Added event listeners and implement caching.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
+//Add and configure workbox plugins for a service worker and manifest file.
 
 module.exports = () => {
   return {
@@ -48,7 +48,7 @@ module.exports = () => {
       
     ],
     
-    // TODO: Add CSS loaders and babel to webpack. DONE
+    //Add CSS loaders and babel to webpack. DONE
     module: {
       rules: [
         {
