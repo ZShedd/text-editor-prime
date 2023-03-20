19686c4e14ee8f2944f075f007dcf6aab22a005a
Clown Supreme <zachary.h.shedd@gmail.com>
2023-03-19 20:05:36 -0700

    Added to gitignore and webpack.config.js in client folder

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,12 +17,28 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      //html weback plugin
     ],
-
+    
+    // TODO: Add CSS loaders and babel to webpack. DONE
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // bable
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@bable/preset-env'],
+              plugins: ['@bable/plugin-proposal-object-rest-spread', '@bable/transform-runtime'],
+            }
+          }
+        }
       ],
     },
   };
